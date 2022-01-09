b576eb3aaf24d6c32209c85768f4c24817c9ee5f
Yash Ajitsaria <ajitsariayash320@gmail.com>
2022-01-09 18:28:08 +0530

    Notifications and MenuBar added

--- lib/Notifications.dart
+++ lib/Notifications.dart
@@ -1,6 +1,10 @@
-// ignore_for_file: file_names, prefer_const_constructors
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'MenuBar.dart';
 
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
@@ -15,16 +19,338 @@ class _NotificationsState extends State<Notifications> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // leading: IconButton(
-        //   onPressed: (){}
-        //   // icon: ImageIcon(),
-        // ),
+        leading: IconButton(
+          onPressed: () {
+            MenuBar();
+          },
+          icon: ImageIcon(
+            AssetImage('UIAssets/menubar.png'),
+          ),
+        ),
         title: Center(
           child: Text(
             'Notifications',
-            style: TextStyle(),
+            style: TextStyle(
+              fontSize: 25,
+            ),
           ),
         ),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'System',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Booking #1234 has been success...',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Promotion',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Invite friends - Get 3 coupons each!',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Promotion',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Invite friends - Get 3 coupons each!',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'System',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Thank you! Your transaction is com...',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'System',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Booking #1234 has been success...',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Promotion',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Invite friends - Get 3 coupons each!',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'System',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Thank you! Your transaction is com...',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              elevation: 0,
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset('UIAssets/time.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Promotion',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Invite friends - Get 3 coupons each!',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
